8f88fe7a278d7a976171be82ebbcc796ef4098a6
net kobo <netkobo1@gmail.com>
2020-06-07 11:42:24 +0900

    ２進数からの変換を実装

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-test file
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>数値変換ツール</title>
+</head>
+<body>
+<?php
+  $dec = null;
+  if (isset($_GET['bin'])) {
+    $bin = $_GET['bin'];
+    $dec = intval($bin, 2);
+  }
+  if (isset($dec)) {
+    $str_result = sprintf("１０進数 %d は、<br>２進数：%b<br>１６進数: %x<br>です。",
+    $dec, $dec, $dec);
+  } else {
+    $str_result = "URLの後ろに ?bin=２進数 のパラメータを付けてアクセスしてみてください。";
+  }
+?>
+
+<p><?= $str_result ?></p>
+
+</body>
+</html>
